ba01f4973b77f3438af08e32af9d21c55f2ad9ff
Vicentecorrea <vicorrea@miuandes.cl>
2019-09-24 12:01:46 -0300

    Added handler accept/reject request

--- Proyecto3/components/FriendRequest.js
+++ Proyecto3/components/FriendRequest.js
@@ -4,16 +4,56 @@ import { View, Text, Dimensions, StyleSheet, Image, TouchableOpacity } from 'rea
 export default class FriendRequest extends Component {
     constructor(props){
         super(props);
+        getUserID = this.getUserID.bind(this);
+    }
+    getUserID(){
+      return this.props.item.user_id
+    }
+    handleAcceptRequest(){
+      fetch(global.domain + '/update_request', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_id: global.user_id,
+          user_sender: getUserID(),
+          state: 2
+        })
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+      })
     }
 
+    handleRejectRequest(){
+      fetch(global.domain + '/update_request', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_id: global.user_id,
+          user_sender: getUserID(),
+          state: 0
+        })
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        
+      })
+    }
 
     render() {
       if(this.props.state == 1){
         return (
                 <View style={styles.container}>
                     <Text>{this.props.item.nickname}</Text>
-                    <TouchableOpacity><Text>Accept</Text></TouchableOpacity>
-                    <TouchableOpacity><Text>Reject</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={this.handleAcceptRequest}><Text>Accept</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={this.handleRejectRequest}><Text>Reject</Text></TouchableOpacity>
                 </View>
         )
       }
